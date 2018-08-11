1acef691b2dc7270b9cd129208612431a351f533
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-10 22:54:20 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/pokedex/ListaActivity.kt
@@ -0,0 +1,42 @@
+package br.com.fiap.pokedex
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
+import br.com.fiap.pokedex.model.Pokemon
+import kotlinx.android.synthetic.main.activity_lista.*
+
+class ListaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista)
+
+        rvPokemons.adapter = ListaPokemonAdapter(getPokemons(),
+                this, {
+            val teladetalhe = Intent(this, Detalhe_Activity::class.java)
+
+            teladetalhe.putExtra("POKEMON", it)
+            startActivity(teladetalhe)
+        })
+
+        rvPokemons.layoutManager = LinearLayoutManager(this)
+    }
+
+
+    fun getPokemons(): List<Pokemon> {
+        return listOf(
+                getMewtwo()
+        )
+    }
+
+    fun getMewtwo(): Pokemon {
+        return Pokemon(1,
+                "Mewtwo",
+                "Psy",
+                "bla bla",
+                R.drawable.mewtwo)
+    }
+}
